dcd2a8693e35afd6f0c16a0c917833db57ab1706
EvanGks <vgakias_@hotmail.com>
2024-11-17 12:37:25 +0200

    Add files via upload

--- Database.js
+++ Database.js
@@ -1,2 +1,10 @@
 //User: root
 //Password: Z6vFqfu6kVDhjXJXoHGuPVxD
+//MONGO_HOST: 172.21.186.170
+//MONGO_PORT: 27017
+//URL: https://labs-mongo-orange-fluffy-umbrella.mongo.databases.labs.skills.network
+//MONGO_URL: https://labs-mongo-orange-fluffy-umbrella.mongo.databases.labs.skills.network
+//MongoDB CLI Command: mongosh mongodb://root:Z6vFqfu6kVDhjXJXoHGuPVxD@172.21.186.170:27017
+//MONGO_COMMAND: mongosh mongodb://root:Z6vFqfu6kVDhjXJXoHGuPVxD@172.21.186.170:27017
+//MONGO_TITLE: MongoDB Database
+//MONGO_ID: labs-mongo-orange-fluffy-umbrella
